8257fcc212ae531ed82cf6d0033535478ee3245c
Jim Burnham <jim.the.steam.clown@gmail.com>
2021-02-01 15:51:05 -0800

    Update nano-2G-dockerSetupUSBCamera.sh

--- nano-2G-dockerSetupUSBCamera.sh
+++ nano-2G-dockerSetupUSBCamera.sh
@@ -1,3 +1,8 @@
+# this is the setup for the USB camera Docker file
+# sudo wget -O ./nano-2G-dockerSetupUSBCamera.sh https://raw.githubusercontent.com/jimTheSTEAMClown/JetsonNano/master/nano-2G-dockerSetupUSBCamera.sh
+# sudo chmod 755 ./nano-2G-dockerSetupUSBCamera.sh
+# sudo cat ./nano-2G-dockerSetupUSBCamera.sh
+
 echo "sudo docker run --runtime nvidia -it --rm --network host \
     --volume ~/nvdli-data:/nvdli-nano/data \
     --device /dev/video0 \
